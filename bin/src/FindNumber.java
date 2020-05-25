84870856884cc15b0c26c1ed7641169bcab02e79
netFontero <59034202+netFontero@users.noreply.github.com>
2020-05-24 20:37:02 -0700

    start

---
+++ bin/src/FindNumber.java
@@ -0,0 +1,25 @@
+
+public class FindNumber 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+	
+    public int findNumbers(int[] nums) 
+    {
+        int isEven = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            int tenDiv = 0;
+            
+            while(nums[i] != 0)
+            {
+                
+            }
+        }
+		return isEven;
+    }
+}
