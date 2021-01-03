a2fbd693c9cab6b0fed46b2adbaa75e88b92c85e
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2021-01-02 18:22:21 -0800

    Update FindNumber.java

--- src/FindNumber.java
+++ src/FindNumber.java
@@ -1,25 +1,18 @@
-
-public class FindNumber 
-{
-
-	public static void main(String[] args) 
-	{
+public class FindNumber {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
  
-    public int findNumbers(int[] nums) 
-    {
+    public int findNumbers(int[] nums) {
         int isEven = 0;
-        for(int i = 0; i < nums.length; i++)
-        {
+        for(int i = 0; i < nums.length; i++){
             int tenDiv = 0;
             
-            while(nums[i] != 0)
-            {
+            while(nums[i] != 0){
                 //make this work
             }
         }
-		return isEven;
+	return isEven;
     }
 }
